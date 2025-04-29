cba9f4cf1586ff00ecbb1308f006b06a15ac1c55
Satria Nugraha <therealsatria@gmail.com>
2025-04-29 10:57:13 +0700

    initial

---
+++ Insfrastructures/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace pmapp.Infrastrctures.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
